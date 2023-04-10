e49f3eb659db5ad898166f37a32a71e6a9a38123
iqbalafaaf <130390465+iqbalafaaf@users.noreply.github.com>
2023-04-10 18:48:51 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta charset="UTF-8">
+    
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Etihad Airways
+        
+    </title>
+   
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap" rel="stylesheet">
+<link rel = "stylesheet" href = "index.css">
+<link rel = "shortcut icon" type= "x-icon" href="logo.png"> 
+
+
+</head>
+<body>
+    <img class= "bg" src="bg.png" alt="Etihad Airways">
+    <div class="container">
+        <b><h1>Welcome to Etihad Airways Winning Contest</h1></b>
+        <p>Win a free Holiday Package to Zurich
+        </p>
+        <div class="form"><form
+  action="https://formspree.io/f/myyabwwr"
+  method="POST"
+>
+      
+
+
+        
+
+        <input type = "text" name="name" id="name" placeholder="Name">
+        
+
+
+        <input type="tel" id="phone" placeholder="Phone Number" >
+     
+        <input type="email" name="email" id="email" placeholder="E-Mail ID">
+
+     
+
+
+        <input type="address" name="address" id="address" placeholder="Address" >
+        
+        <textarea name="desc" id="desc" cols="30" rows="10" placeholder="Other Information"></textarea>
+    <button class="btn">Submit</button>
+   
+</form>
+</div>
+    </div>
+    <script src="index.js"></script> 
+   
+</body>
+
+</html>
